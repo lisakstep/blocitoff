f9083ea4c2ef3cdc397566fde47b52e69491906b
lisakstep <lisakstep@gmail.com>
2014-11-23 20:32:06 +0000

    initial commit for Blocitoff

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is a to-do list which uses rake automation to delete list items after a set amount of time.
+
+
+Authored with my mentor Xander Miller at [Bloc](http://bloc.io).
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocitoff::Application.config.secret_key_base = '2da0f4193b28aab5c7a81306dceee89b81083fb371365b1c3a99f13130a7eff8f739a25ae83a568734af3df74fe464d32c114f2b63d68f22127cb8fd18c0cfcd'
